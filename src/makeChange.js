93ed0292f1ada6db63e6b3239b9aba0a52eb4e5f
smithrm941 <smithrm941@gmail.com>
2017-02-27 14:55:58 -0800

    makeChange finished

--- src/makeChange.js
+++ src/makeChange.js
@@ -1,3 +1,24 @@
 export default function makeChange({price, amountGiven}) {
-  // your code here
+  const change = {
+    quarters: 0,
+    dimes: 0,
+    nickels: 0,
+    pennies: 0
+  }
+  const totalChange = amountGiven - price;
+  var difference = amountGiven - price;
+    if(difference > 24){
+      change.quarters = Math.floor(difference/25);
+      difference = difference - 25*change.quarters;
+    }
+    if(difference > 9){
+      change.dimes = Math.floor(difference/10);
+      difference = difference - 10*change.dimes;
+    }
+    if(difference > 4){
+      change.nickels = Math.floor(difference/5);
+      difference = difference - 5*change.nickels;
+    }
+    change.pennies = difference;
+  return change;
 }
