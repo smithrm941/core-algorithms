5ce3e8e7a05ccb3f9465f003e4afd9cac449b257
Cadence Case <cadencecase@Cadences-MacBook-Pro.local>
2017-02-28 10:17:30 -0800

    implement fizzBuzz WIP

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,3 +1,18 @@
 export default function fizzBuzz(){
-
+  var array = [];
+    for (var i = 1; i < num + 1; i++){
+      array.push(i);
+    }
+    for (var i = 0; i<array.length; i++){
+      if (array[i] % 3 === 0 && array[i] % 5 !== 0){
+        array.splice(i, 1, 'Fizz');
+      }
+      if (array[i] % 5 === 0 && array[i] % 3 !== 0){
+        array.splice(i, 1, 'Buzz');
+      }
+      if (array[i] % 3 === 0 && array[i] % 5 === 0){
+        array.splice(i, 1, 'FizzBuzz');
+      }
+    }
+    return array;
 }
