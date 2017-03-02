55f4f523cb854ce475a82a33b3be187d961eb969
CadenceCase <cadencecase@gmail.com>
2017-03-02 10:29:51 -0800

    adds tests for invalid inputs, refactors function

--- test/isPalindrome_test.js
+++ test/isPalindrome_test.js
@@ -1,18 +1,22 @@
 import { expect } from 'chai'
 import isPalindrome from '../src/isPalindrome'
 
-describe('isPalindrome(inputString)', function(){
-  
+describe('isPalindrome(input)', function(){
   it('should be a function', function(){
     expect(isPalindrome).to.be.a('function')
   })
 
-  it('returns true if inputString is palindrome', function(){
+  it('returns true if input is palindrome', function(){
     expect(isPalindrome('racecar')).to.deep.equal(true)
     expect(isPalindrome('A man, a plan, a canal: Panama')).to.deep.equal(true)
   })
 
-  it('returns false if inputString is not palindrome', function(){
+  it('returns false if input is not palindrome', function(){
     expect(isPalindrome('lemon')).to.deep.equal(false)
+  })
+
+  it('should return error message if argument is not a string', function(){
+    expect(isPalindrome([1, 2, 3])).to.equal('Invalid input')
+    expect(isPalindrome(1)).to.equal('Invalid input')
   })
 })
