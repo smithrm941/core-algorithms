54ea1eafebeace532d72f7595edf77eab2d35e1f
CadenceCase <cadencecase@gmail.com>
2017-03-03 10:54:15 -0800

    refactors src file for fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,17 +1,13 @@
-export default function fibonacci(n){
-  if(n === 1){
+export default function fibonacci(num){
+  if (num === 1) {
     return [0];
-  }else if(n > 1){
-    var array = [0, 1];
-    var tempArray = [];
-    while(n>2){
-      tempArray = (array.slice(-2));
-      var nextTerm = tempArray.reduce(function(a,b) {return a+b;});
-      array.push(nextTerm);
-      n--;
+  } else if (num > 1) {
+    var fibonacciArray = [0, 1];
+    while (num > 2) {
+      var nextTerm = fibonacciArray.slice(-2).reduce(function(a,b) {return a+b;});
+      fibonacciArray.push(nextTerm);
+      num--;
     }
-    return array;
-  }else{
-    return undefined;
+    return fibonacciArray;
   }
 }
