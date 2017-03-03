e56a49143509d1572d3af9bfdc5b42d5f1b571fa
CadenceCase <cadencecase@gmail.com>
2017-03-03 11:08:03 -0800

    adds test that contains additional characters

--- test/isPalindrome_test.js
+++ test/isPalindrome_test.js
@@ -9,6 +9,7 @@ describe('isPalindrome(input)', function(){
   it('returns true if input is palindrome', function(){
     expect(isPalindrome('racecar')).to.deep.equal(true)
     expect(isPalindrome('A man, a plan, a canal: Panama')).to.deep.equal(true)
+    expect(isPalindrome('"Are _we_ not pure? “No, sir!” Panama’s moody Noriega brags. “It is garbage!” Irony dooms a man—-a prisoner up to new era."')).to.deep.equal(true)
   })
 
   it('returns false if input is not palindrome', function(){
