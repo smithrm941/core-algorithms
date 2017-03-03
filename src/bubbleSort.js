e649ea73f1f44a998debe63e7cd5c9ca016894cf
smithrm941 <smithrm941@gmail.com>
2017-03-03 09:49:33 -0800

    adds src and test file for bubbleSort all tests passing

--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -1,39 +1,26 @@
-// export default function bubbleSort(array){
-//   var sortingHat = [];
-//   if(Array.isArray(array)){ //this checks for an array
-//     for(var i = 0; i<array.length; i++){ //checks that the array contains only numbers
-//       if(isNaN(array[i]) === false){
-//         sortingHat.push(array[i]);
-//       }//pushes all numerical elements to a new array and allows for statement to loop
-//           }else{
-//         return 'Please enter an array of numbers'; //sends error message if non-number is in array
-//       }
-//   }else{
-//     return 'Please enter an array of numbers'; //sends error message if argument is not an array
-//   }
-// }
-
 export default function bubbleSort(array){
   var sortingHat = [];
-  if(Array.isArray(array)){
+  if(Array.isArray(array)) {
     for(var i = 0; i<array.length; i++){
       if(isNaN(array[i]) === false){
         sortingHat.push(array[i]);
-      }
-      }else{
+      } else {
         return 'Please enter an array of numbers';
-    }else{
-      return 'Please enter an array of numbers';
+      }
     }
+  } else {
+    return 'Please enter an array of numbers';
   }
+  sortingHat.forEach(function repeater(element){
+    for(var x = 0; x<sortingHat.length; x++){
+      while(sortingHat[x] > sortingHat[x+1]){
+        if(sortingHat[x] > sortingHat[x+1]){
+          var temp = sortingHat[x];
+          sortingHat[x] = sortingHat[x+1];
+          sortingHat[x+1] = temp;
+        }
+      }
+    }
+  });
+  return sortingHat;
 }
-
-//console.log(sortingHat);
-
-// }
-//   }
-  // while(i<sortingHat.length){ //swaps adjacent values one time
-  //   if(sortingHat[i] > sortingHat[i+1]){
-  //     var temp = sortingHat[i];
-  //     sortingHat[i] = sortingHat[i+1];
-  //     sortingHat[i+1] = temp;
